b648968489ce554882488013d5311a385000a36f
Milton Paredes <mhplur@hotmail.com>
2019-09-19 16:25:28 -0500

    Sort elements in array of integers

--- sortElementsArray.java
+++ sortElementsArray.java
@@ -1,18 +1,11 @@
-public class InsertSort {
-    public static void main (String [] args) {
-        int [] array = {45,12,85,32,89,39,69,44,42,1,6,8};
-        int temp;
-        for (int i = 1; i < array.length; i++) {
-            for (int j = i; j > 0; j--) {
-                if (array[j] < array [j - 1]) {
-                    temp = array[j];
-                    array[j] = array[j - 1];
-                    array[j - 1] = temp;
-                }
+public static int[] ordenarElementos( int[] arrayEnteros) {
+    int elementoTemporal;
+    for (int i = 1; i < arrayEnteros.length; i++) 
+        for (int j = i; j > 0; j--) 
+            if (arrayEnteros[j] < arrayEnteros[j - 1]) {
+                elementoTemporal = arrayEnteros[j];
+                arrayEnteros[j] = arrayEnteros[j - 1];
+                arrayEnteros[j - 1] = elementoTemporal;
             }
-       }
-       for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
-       }
-    }
+    return arrayEnteros;
 }
